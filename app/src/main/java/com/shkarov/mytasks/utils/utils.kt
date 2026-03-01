27f0679ee05b6cba16262fa0895469f973f0d4ba
Ivan <shkarov_ivan@mail.ru>
2026-03-01 22:38:37 +0300

    try date formatter

--- app/src/main/java/com/shkarov/mytasks/utils/utils.kt
+++ app/src/main/java/com/shkarov/mytasks/utils/utils.kt
@@ -1,5 +1,6 @@
 package com.shkarov.mytasks.utils
 
+import timber.log.Timber
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -9,7 +10,13 @@ fun String.toEpochMillis(
     pattern: String = "yyyy-MM-dd HH:mm:ss",
     zone: ZoneId = ZoneId.systemDefault()
 ): Long {
-    val formatter = DateTimeFormatter.ofPattern(pattern)
-    val ldt = LocalDateTime.parse(this, formatter)
-    return ldt.atZone(zone).toInstant().toEpochMilli()
+    try {
+        val formatter = DateTimeFormatter.ofPattern(pattern)
+        val ldt = LocalDateTime.parse(this, formatter)
+        return ldt.atZone(zone).toInstant().toEpochMilli()
+    } catch (e: Exception) {
+        Timber.e("Ошибка парсинга даты: ${e.message}")
+        return 0L
+    }
+
 }
